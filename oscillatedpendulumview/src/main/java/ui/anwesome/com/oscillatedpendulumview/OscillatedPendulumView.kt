a9958fb18ce56a4ec80552524b86da4fc2d5dcfd
Anwesh43 <anweshthecool0@gmail.com>
2018-01-22 07:32:41 +0530

    adding OscillatedPendulum

--- oscillatedpendulumview/src/main/java/ui/anwesome/com/oscillatedpendulumview/OscillatedPendulumView.kt
+++ oscillatedpendulumview/src/main/java/ui/anwesome/com/oscillatedpendulumview/OscillatedPendulumView.kt
@@ -19,4 +19,22 @@ class OscillatedPendulumView(ctx:Context):View(ctx) {
         }
         return true
     }
+    data class OscillatedPendulum(var x:Float,var y:Float,var l:Float,var r:Float) {
+        fun draw(canvas:Canvas,paint:Paint) {
+            paint.color = Color.parseColor("#4527A0")
+            canvas.save()
+            canvas.translate(x,y)
+            canvas.rotate(90f)
+            paint.strokeWidth = r/7
+            canvas.drawLine(0f,0f,0f,l,paint)
+            canvas.drawCircle(0f,l+r,r,paint)
+            canvas.restore()
+        }
+        fun update(stopcb:()->Unit) {
+
+        }
+        fun startUpdating(startcb:()->Unit) {
+
+        }
+    }
 }
