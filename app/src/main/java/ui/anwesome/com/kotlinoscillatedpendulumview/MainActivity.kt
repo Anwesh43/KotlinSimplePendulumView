230f6246df320c57488fc8ebdca95616c723ac08
Anwesh43 <anweshthecool0@gmail.com>
2018-01-22 08:13:41 +0530

    using OscillatedPendulumView in activity

--- app/src/main/java/ui/anwesome/com/kotlinoscillatedpendulumview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlinoscillatedpendulumview/MainActivity.kt
@@ -2,11 +2,12 @@ package ui.anwesome.com.kotlinoscillatedpendulumview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import ui.anwesome.com.oscillatedpendulumview.OscillatedPendulumView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val view = OscillatedPendulumView.create(this)
     }
 }
