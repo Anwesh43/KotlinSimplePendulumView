f9f66ff39b901d8788a23d3b15b53b7ed9ca6583
Anwesh43 <anweshthecool0@gmail.com>
2018-01-22 07:55:52 +0530

    using renderer in view and creating static create method

--- oscillatedpendulumview/src/main/java/ui/anwesome/com/oscillatedpendulumview/OscillatedPendulumView.kt
+++ oscillatedpendulumview/src/main/java/ui/anwesome/com/oscillatedpendulumview/OscillatedPendulumView.kt
@@ -9,13 +9,14 @@ import android.content.*
 import android.graphics.*
 class OscillatedPendulumView(ctx:Context):View(ctx) {
     val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    val renderer = OscillatedPendulumRenderer(this)
     override fun onDraw(canvas:Canvas) {
-
+        renderer.render(canvas,paint)
     }
     override fun onTouchEvent(event:MotionEvent):Boolean {
         when(event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap()
             }
         }
         return true
